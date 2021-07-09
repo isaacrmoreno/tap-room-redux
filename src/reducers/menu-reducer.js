b53a98d7bbdcbf516dd8b1759300f0b08fc0dceb
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-09 10:07:02 -0700

    :white_check_mark: ADD_BEER test pass

--- src/reducers/menu-reducer.js
+++ src/reducers/menu-reducer.js
@@ -1,4 +1,18 @@
 
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, price, alcoholContent, id } = action;
+  switch (action.type) {
+  case 'ADD_BEER':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
 };
