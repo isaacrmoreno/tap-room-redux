1f9a9ef546d980c756126cf5d15a4c1fcc4e88c6
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-09 15:25:54 -0700

    :lipstick: added row and column to make style form

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Header from './Header';
 import PubControl from './PubControl'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 function App() {
   return (
     <React.Fragment>
       <Container>
         <Header/>
+        <Row className="justify-content-md-center">
+          <Col sm={8}>
         <PubControl/>
+          </Col>
+        </Row>
       </Container>
     </React.Fragment>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'; 
 
+
 function Header(){
   return (
     <React.Fragment>
-      <h1 className="header-copy"><span role="img" aria-label="Clinking Beer Mugs">🍻</span> Tap Room Le Royal <span role="img" aria-label="Crown">👑</span></h1>
+      <h1><span role="img" aria-label="Clinking Beer Mugs">🍻</span> Tap Room Le Royal <span role="img" aria-label="Crown">👑</span></h1>
     </React.Fragment>
   )
 }
