a80f4b9a00742e8a7ab27c75c93dac3002296c0b
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-09 15:36:01 -0700

    :zap: add required attribute to Reusable form input fields

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -9,16 +9,16 @@ function ReusableForm(props) {
       <Form onSubmit={props.formSubmissionHandler}>
   <Form.Group className="formInput" >
     <Form.Label>Input New Keg Details:</Form.Label>
-    <Form.Control type="text" name="name" placeholder="Name:" />
+    <Form.Control type="text" name="name" placeholder="Name:" required="true"/>
   </Form.Group>
   <Form.Group className="formInput">
-    <Form.Control type="text" name="brand" placeholder="Brand:" />
+    <Form.Control type="text" name="brand" placeholder="Brand:" required="true"/>
   </Form.Group>
   <Form.Group className="formInput" >
-    <Form.Control type="text" name="price" placeholder="Price:" />
+    <Form.Control type="text" name="price" placeholder="Price:" required="true"/>
   </Form.Group>
   <Form.Group className="formInput">
-    <Form.Control type="text" name="alcoholContent" placeholder="ABV:" />
+    <Form.Control type="text" name="alcoholContent" placeholder="ABV:" required="true"/>
   </Form.Group>
   <Button variant="primary" size="lg" type='submit'>{props.buttonText}</Button>
   </Form>
