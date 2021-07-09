2e634735a76a83acab7d2a7080b460d7c8f1f193
Isaac Moreno <ipdxcreative@gmail.com>
2021-07-09 11:49:37 -0700

    RED: good fail

--- src/__test__/actions/index.test.js
+++ src/__test__/actions/index.test.js
@@ -8,4 +8,11 @@ describe('Tap Room Le Royal actions', () => {
       id: 1
     });
   });
+
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
+
 });
